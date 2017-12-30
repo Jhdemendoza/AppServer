c7c31c7e9ab08879c9b454a0054bd9d67ec516f7
Jhdemendoza <jaime.hurtadodemendoza.sastre@alumnos.upm.es>
2017-12-30 18:18:17 +0100

    install CRM v3

--- setEnvCRM.py
+++ setEnvCRM.py
@@ -3,8 +3,8 @@
 import os
 
 file = open('/etc/environment', 'a')
-file.write('DATABASE_URL=postgres://crm:xxxx@10.1.4.31:5432/crm')
+file.write('DATABASE_URL=postgres://crm:xxxx@10.1.4.31:5432/crm\n')
 file.close()
 file = open('/etc/environment', 'a')
-file.write('PORT=80')
+file.write('PORT=80\n')
 file.close()
